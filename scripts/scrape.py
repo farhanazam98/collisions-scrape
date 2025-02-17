4ccbcfabbfc9255dfe8c6a99ff0f64a322dcf45a
farhanazam98 <farhanazam98@yahoo.com>
2025-02-17 13:55:23 -0500

    fetch net new crashes

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -6,19 +6,21 @@ import json
 from district_helper import load_data, assign_districts
 
 try:
+    latest_data_file = "../data/latest_collisions.csv"
+    df_existing = pd.read_csv(latest_data_file, usecols=["collision_id"])
+    latest_collision_id = df_existing["collision_id"].max()  # Get latest ID
+    print(latest_collision_id)
+
     client = Socrata("data.cityofnewyork.us", None)
     
     today = datetime.now().strftime('%Y-%m-%d')
     
-    # only pulling 1K for now. will probably need a key to bump this up? 
-    query = """
-    SELECT *
-    ORDER BY crash_date DESC
-    LIMIT 1000
-    """
+    initial_date = "2025-02-11"  
+    query = f"SELECT * WHERE collision_id > {latest_collision_id} ORDER BY crash_date DESC LIMIT 1000"
+
     
     results = client.get("h9gi-nx95", query=query)
-    
+
     if not results:
         print("Error: No data retrieved!")
         sys.exit(1)
@@ -33,13 +35,13 @@ try:
     print(f"Date range: {df['crash_date'].min()} to {df['crash_date'].max()}")
     print(f"Boroughs represented: {df['borough'].unique().tolist()}")
     
-    output_file = 'latest_collisions.csv'
+    output_file = '../data/latest_collisions.csv'
     df.to_csv(output_file, index=False)
     print(f"Data saved to {output_file}")
 
      # Assign districts using district_helper
-    collision_file = 'latest_collisions.csv'
-    boundary_file = 'city_council_boundaries.csv'
+    collision_file = '../data/latest_collisions.csv'
+    boundary_file = '../data/city_council_boundaries.csv'
     
     try:
         collisions_gdf, boundaries_gdf = load_data(collision_file, boundary_file)
